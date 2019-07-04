b0cdc3a0feb2e7db78c8030a3bdbb66e4bdd7e40
Dustin Williams <cdustinwilliams@gmail.com>
2019-07-03 21:45:35 -0400

    Apparently I don't know how Git works

--- cube.py
+++ cube.py
@@ -105,118 +105,260 @@ class Cube:
         """
         """
         Facet definitions on unwrapped cube:
-        X  X  X   X  X  X   1  2  3   X  X  X
-        X  X  X   X  X  X   4  U  5   X  X  X
-        X  X  X   X  X  X   6  7  8   X  X  X
+        X  X  X   X  X  X   0  1  2   X  X  X
+        X  X  X   X  X  X   3  U  4   X  X  X
+        X  X  X   X  X  X   5  6  7   X  X  X
 
-       41 42 43   9 10 11  17 18 19  25 26 27
-       44 B  45  12 L  13  20 F  21  28 R  29
-       46 47 48  14 15 16  22 23 24  30 31 32
+       40 41 42   8 9  10  16 17 18  24 25 26
+       43 B  44  11 L  12  19 F  20  27 R  28
+       45 46 47  13 14 15  21 22 23  29 30 31
 
-        X  X  X   X  X  X  33 34 35   X  X  X
-        X  X  X   X  X  X  36 D  37   X  X  X
-        X  X  X   X  X  X  38 39 40   X  X  X
+        X  X  X   X  X  X  32 33 34   X  X  X
+        X  X  X   X  X  X  35 D  36   X  X  X
+        X  X  X   X  X  X  37 38 39   X  X  X
         """
-        self.cube_dict = {
-            "edges": {
-                # Up
-                "UB": Edge([2, 42]),
-                "UL": Edge([4, 10]),
-                "UR": Edge([5, 26]),
-                "UF": Edge([7, 18]),
-                # Front
-                "FL": Edge([20, 13]),
-                "FR": Edge([21, 28]),
-                # Back
-                "BL": Edge([45, 12]),
-                "BR": Edge([44, 29]),
-                # Down
-                "DB": Edge([39, 47]),
-                "DL": Edge([36, 15]),
-                "DR": Edge([37, 31]),
-                "DF": Edge([34, 23])
-            },
-            "corners": {
-                # Up
-                "UBL": Corner([1, 9, 43]),
-                "UBR": Corner([3, 41, 27]),
-                "UFL": Corner([6, 17, 11]),
-                "UFR": Corner([8, 25, 19]),
-                # Down
-                "DBL": Corner([38, 48, 14]),
-                "DBR": Corner([40, 32, 46]),
-                "DFL": Corner([33, 16, 22]),
-                "DFR": Corner([35, 24, 30])
-            }
-        }
+        # self.cube_dict = {
+        #     "edges": {
+        #         # Up
+        #         "UB": Edge([2, 42]),
+        #         "UL": Edge([4, 10]),
+        #         "UR": Edge([5, 26]),
+        #         "UF": Edge([7, 18]),
+        #         # Front
+        #         "FL": Edge([20, 13]),
+        #         "FR": Edge([21, 28]),
+        #         # Back
+        #         "BL": Edge([45, 12]),
+        #         "BR": Edge([44, 29]),
+        #         # Down
+        #         "DB": Edge([39, 47]),
+        #         "DL": Edge([36, 15]),
+        #         "DR": Edge([37, 31]),
+        #         "DF": Edge([34, 23])
+        #     },
+        #     "corners": {
+        #         # Up
+        #         "UBL": Corner([1, 9, 43]),
+        #         "UBR": Corner([3, 41, 27]),
+        #         "UFL": Corner([6, 17, 11]),
+        #         "UFR": Corner([8, 25, 19]),
+        #         # Down
+        #         "DBL": Corner([38, 48, 14]),
+        #         "DBR": Corner([40, 32, 46]),
+        #         "DFL": Corner([33, 16, 22]),
+        #         "DFR": Corner([35, 24, 30])
+        #     }
+        # }
 
-    def move(self, movement: str):
-        c = CubeMovement(self, movement)
-        c.move()
+        self.facet_list = list(range(48))  # Each item represents a facet
+        # Locations can be referred to by a normal range(48)
 
-    def swap(self, piece1, piece2):
-        corners = self.cube_dict["corners"]
-        edges = self.cube_dict["edges"]
-        if piece1 in corners.keys() and piece2 in corners.keys():
-            temp = corners[piece1]
-            corners[piece1] = corners[piece2]
-            corners[piece2] = temp
-        elif piece1 in edges.keys() and piece2 in edges.keys():
-            temp = edges[piece1]
-            edges[piece1] = edges[piece2]
-            edges[piece2] = temp
+    def __eq__(self, other):
+        return self.facet_list == other.facet_list
+
+    def swap(self, location1: int, location2: int, *args) -> None:
+        # Swaps two facets. If more than 2 facets, shifts them right (e.g., swap(A, B, C), A to B, B to C, C to A)
+        cycle = [location1, location2]
+        cycle = cycle + list(args)
+        # Error checking:
+        for i in cycle:
+            try:
+                # TODO: Are these numbers too magic?
+                if not 0 <= i <= 47 or type(i) != int:
+                    raise TypeError
+            except TypeError:
+                raise TypeError("Error in term {}: Expected int, received {}".format(i, type(i)))
+        if len(cycle) != len(set(cycle)):  # Duplicate checking
+            raise ValueError("Swaps cannot have duplicate items")
+
+        pos1 = cycle[0]  # just swaps with first element
+        for i in range(len(cycle) - 1):  # Only need n-1 loops since we'll be adding 1 to i
+            pos2 = cycle[i+1]
+            temp_value = self.facet_list[pos2]
+            self.facet_list[pos2] = self.facet_list[pos1]
+            self.facet_list[pos1] = temp_value
+
+    # def swap(self, piece1, piece2):
+    #     corners = self.cube_dict["corners"]
+    #     edges = self.cube_dict["edges"]
+    #     if piece1 in corners.keys() and piece2 in corners.keys():
+    #         temp = corners[piece1]
+    #         corners[piece1] = corners[piece2]
+    #         corners[piece2] = temp
+    #     elif piece1 in edges.keys() and piece2 in edges.keys():
+    #         temp = edges[piece1]
+    #         edges[piece1] = edges[piece2]
+    #         edges[piece2] = temp
+    #     else:
+    #         raise ValueError("Pieces must be either edges or corners.")
+
+    def move(self, movement, reverse=False):
+        movement_ = 0
+        if type(movement) == str:
+            movement_ = CubeMovement(movement)
+        elif type(movement) == CubeMovement:
+            movement_ = movement
         else:
-            raise ValueError("Pieces must be either edges or corners.")
+            raise TypeError("Invalid type {}, expected 'str' or 'CubeMovement'".format(type(movement)))
+
+        m = movement_.move[0]
+        inverted = movement_.inverted
+        double = movement_.double
+
+        swap_list = dict()
+        swap_list["U"] = [[1, 4, 6, 3],  # U edges
+                          [0, 2, 7, 5],  # U corners
+                          [17, 9, 41, 25],   # U side edges
+                          [16, 8, 40, 24],   # U side corners #1
+                          [18, 10, 42, 26]]  # U side corners #2
+        swap_list["D"] = [[33, 36, 38, 35],  # D edges
+                          [32, 34, 39, 37],  # D corners
+                          [22, 30, 46, 14],  # D side edges
+                          [13, 21, 29, 45],  # D side corners #1
+                          [47, 15, 23, 31]]  # D side corners #2
+        swap_list["L"] = [[9, 12, 14, 11],  # etc
+                          [8, 10, 15, 13],
+                          [3, 19, 35, 44],
+                          [0, 16, 32, 47],
+                          [5, 21, 37, 42]]
+        swap_list["R"] = [[25, 28, 30, 27],  # etc
+                          [24, 26, 31, 29],
+                          [4, 43, 36, 20],
+                          [7, 40, 39, 23],
+                          [2, 45, 34, 18]]
+        swap_list["F"] = [[17, 20, 22, 19],  # etc
+                          [16, 18, 21, 23],
+                          [6, 27, 33, 12],
+                          [5, 24, 34, 15],
+                          [7, 29, 32, 10]]
+        swap_list["B"] = [[41, 44, 46, 43],  # etc
+                          [40, 42, 47, 45],
+                          [1, 11, 38, 28],
+                          [2, 8, 37, 31],
+                          [0, 13, 39, 26]]
+
+        swap_face = swap_list[m]
+        inverted = inverted ^ reverse  # If reverse=True, do the opposite of what inverted is.
+        if inverted:
+            for list_ in swap_face:
+                list_.reverse()
+
+        for list_ in swap_face:
+            self.swap(*list_)
+            if double:
+                self.swap(*list_)
+
 
 class CubeMovement:
-
-    class Move(Enum):
-        UP, LEFT, FRONT, RIGHT, DOWN, BACK = range(6)
-
-    def __init__(self, cube: Cube, movement: str):
-        self.cube = cube
+    def __init__(self, movement: str):
         self.inverted = False
-        if movement[-1] == "'":  # Move inverted
-           self.movement = movement[:-1]  # Removes ' symbol
-           self.inverted = True
-        if movement.lower() in ["u", "up"]:
-            self.movement = CubeMovement.Move.UP
-        elif movement.lower() in ["d", "down"]:
-            self.movement = CubeMovement.Move.DOWN
-        elif movement.lower() in ["l", "left"]:
-            self.movement = CubeMovement.Move.LEFT
-        elif movement.lower() in ["r", "right"]:
-            self.movement = CubeMovement.Move.RIGHT
-        elif movement.lower() in ["f", "front"]:
-            self.movement = CubeMovement.Move.FRONT
-        elif movement.lower() in ["b", "back"]:
-            self.movement = CubeMovement.Move.BACK
+        self.double = False
+        self.move = ""
 
-    def move(self):
-        if self.movement == CubeMovement.Move.UP:
-            self.move_up()
-        elif self.movement == CubeMovement.Move.DOWN:
-            self.move_down()
-        elif self.movement == CubeMovement.Move.LEFT:
-            self.move_left()
-        elif self.movement == CubeMovement.Move.RIGHT:
-            self.move_right()
-        elif self.movement == CubeMovement.Move.FRONT:
-            self.move_front()
-        elif self.movement == CubeMovement.Move.BACK:
-            self.move_back()
+        if len(movement) > 2:
+            raise ValueError("Invalid movement, only one move supported")
 
-    def move_up(self):
-        if self.inverted:
-            pass
-        else:
+        move = list(movement)[0]
+        if move not in list("UDFBLRXYZ"):
+            raise ValueError("Error resolving movement")
 
-            pass
+        if len(movement) == 2:
+            mod = list(movement)[-1]
+            if mod == "'":
+                self.inverted = True
+            elif mod == "2":
+                self.double = True
+            else:
+                raise ValueError("Invalid second character, "'" and "2" supported')
+
+        self.move = movement
+
+    def __repr__(self):
+        return "<Move: {}>".format(self.move)
+
+    def __str__(self):
+        return self.move
+
+
+class Algorithm:
+    def __init__(self, algorithm_string, cube):
+        self._string = algorithm_string  # Valid algorithms are space-delimited, e.g., "R' U R U'"
+        self._cube = cube
+        self._step = 0
+
+        self.string = ""
+        self.algorithm = []
+        self.set_algorithm(self._string)
+
+    def __str__(self):
+        return self.string
+
+    def set_algorithm(self, string):
+        str_list = string.split(" ")
+        for s in str_list:
+            try:
+                self.algorithm.append(CubeMovement(s))
+            except ValueError as e:
+                self.algorithm = []  # We want this to be empty if it fails at making the alg
+                raise ValueError(e)
+
+        self.string = self._string = " ".join(str_list)
+
+    def execute(self):
+        for move in self.algorithm:
+            self.step()
+        print_face(self._cube)
+
+    def get_step(self):
+        return self._step
+
+    def step(self):
+        if self._step >= len(self.algorithm):
+            return  # Can't go forward any more  TODO: Should this be an exception?
+        current_step = self.algorithm[self._step]
+        self._cube.move(current_step)
+        self._step += 1
+
+    def step_back(self):
+        if self._step <= 0:
+            return  # Can't go back any more  TODO: Should this be an exception?
+        self._step -= 1
+        current_step = self.algorithm[self._step]
+        self._cube.move(current_step, reverse=True)
+
+    def is_executing(self):
+        if 0 < self._step < len(self.algorithm):
+            return True
+        return False
+
+    def is_finished(self):
+        # Only available when stepping through algorithm, execute() resets to 0
+        return self._step >= len(self.algorithm)
+
 
 def get_color(subfacet: int) -> Color:
     # Each face has 8 subfacets (excluding centers)
-    # Floor dividing a subfacet (subtracted by 1) by 8 and adding 1 gives the face it belongs to
+    # Floor dividing a subfacet by 8 and adding 1 gives the face it belongs to
     # For example, the color of subfacet 36 can be found like this:
-    # (36 - 1) // 8 + 1 == 5, so the color of 36 is the color of the 5th face (default yellow)
-    subfacet -= 1  # Corrects off-by-one error (facets not zero indexed)
+    # 36 // 8 + 1 == 5, so the color of 36 is the color of the 5th face (default yellow)
     return Color(subfacet // 8 + 1)
+
+
+def print_face(cube: Cube):
+    st = ["UP", "LEFT", "FRONT", "RIGHT", "DOWN", "BACK"]
+    sides = 6
+    for i in range(sides):
+        face = []
+        face = cube.facet_list[i * 8:i * 8 + 8]
+        print("{}:".format(st[i]))
+        print("{} {} {}\n{} A {}\n{} {} {}".format(*face))
+
+
+if __name__ == '__main__':
+    # R2 = CubeMovement("R2")
+    # cb = Cube()
+    # cb.move(R2)
+    # print_face(cb)
+    c = Cube()
+    a = Algorithm("R U R' U R U2 R'", c)
+    print_face(c)
--- tests/test.py
+++ tests/test.py
@@ -8,18 +8,10 @@ class TestCubePieces(unittest.TestCase):
         self.COLORS = cube.Color
 
     def test_solved_cube_correct_number_of_facets(self):
-        facets = []
-
-        for k, v in self.cube.cube_dict.items():
-            if k == "corners":
-                for k, corner in v.items():
-                    facets += corner._corner
-            if k == "edges":
-                for k, edge in v.items():
-                    facets += edge._edge
-
+        facets = self.cube.facet_list.copy()
         facets.sort()
-        self.assertEqual(facets, list(range(1, 49)))
+
+        self.assertEqual(facets, list(range(48)))
 
     def test_facets_have_correct_corresponding_color(self):
         # White = top
@@ -33,19 +25,112 @@ class TestCubePieces(unittest.TestCase):
         self.assertEqual(cube.get_color(20), cube.Color.BLUE)
         self.assertEqual(cube.get_color(41), cube.Color.GREEN)
         self.assertEqual(cube.get_color(47), cube.Color.GREEN)
-        self.assertEqual(cube.get_color(40), cube.Color.YELLOW)
+        self.assertEqual(cube.get_color(39), cube.Color.YELLOW)
         self.assertEqual(cube.get_color(3), cube.Color.WHITE)
 
 
-class TestCubeManipulation(unittest.TestCase):
+class TestPieceSwap(unittest.TestCase):
     def setUp(self) -> None:
-        self.manipulated_cube = cube.Cube()
-        self.test_case_cube = cube.Cube()
-        self.test_case_cube.cube_dict["corners"]["UBL"] = cube.Corner([33, 16, 22])
-        self.test_case_cube.cube_dict["corners"]["DFL"] = cube.Corner([1, 9, 43])
+        self.cube = cube.Cube()
+        # self.facet_check_list = list(range(48))
+        # self.facet_check_list[1] = 2
+        # self.facet_check_list[2] = 1
 
-    def test_swap_corners_UBL_DFL(self):
-        self.manipulated_cube.swap("UBL", "DFL")
+    def test_swap_two_facets(self):
+        self.cube.swap(1, 2)
+        self.assertEqual(self.cube.facet_list[2], 1)
+        self.assertEqual(self.cube.facet_list[1], 2)
+
+    def test_swap_three_facets(self):
+        self.cube.swap(22, 23, 24)
+        self.assertEqual(self.cube.facet_list[22], 24)
+        self.assertEqual(self.cube.facet_list[23], 22)
+        self.assertEqual(self.cube.facet_list[24], 23)
+
+    def test_swap_8_facets(self):
+        self.cube.swap(38, 39, 40, 41, 42, 43, 44, 45)
+        self.assertEqual(self.cube.facet_list[38], 45)
+        self.assertEqual(self.cube.facet_list[39], 38)
+        self.assertEqual(self.cube.facet_list[43], 42)
+
+    def test_passing_wrong_type(self):
+        self.assertRaises(TypeError, self.cube.swap, 1, 2, "g")
+
+    def test_passing_duplicate_values(self):
+        self.assertRaises(ValueError, self.cube.swap, 1, 16, 33, 16)
+
+
+class TestCubeMovement(unittest.TestCase):
+    # TODO: Add test cube movement cases to make sure movements are actually correct
+    pass
+
+
+class TestAlgorithmClass(unittest.TestCase):
+    def setUp(self) -> None:
+        self.cube = cube.Cube()
+        self.alg = cube.Algorithm("R U R' U'", self.cube)
+
+    def test_step_forward_then_backward(self):
+        default_cube = cube.Cube()
+        self.alg.step()
+        self.alg.step_back()
+        self.assertEqual(default_cube, self.cube)
+
+    def test_is_executing(self):
+        self.alg.step()
+        self.assertTrue(self.alg.is_executing())
+        self.alg.step()
+        self.alg.step()
+        self.alg.step()
+        self.assertFalse(self.alg.is_executing())
+        self.alg.step_back()
+        self.assertTrue(self.alg.is_executing())
+
+    def test_is_finished(self):
+        #alg = cube.Algorithm("R U R' U'", self.cube)
+        # Who even uses loops
+        self.assertFalse(self.alg.is_finished())
+        self.alg.step()
+        self.assertFalse(self.alg.is_finished())
+        self.alg.step()
+        self.assertFalse(self.alg.is_finished())
+        self.alg.step()
+        self.assertFalse(self.alg.is_finished())
+        self.alg.step()
+        self.assertTrue(self.alg.is_finished())
+
+    def test_steps_too_far(self):
+        # alg = cube.Algorithm("R U R' U'", self.cube)
+        self.alg.step()
+        self.alg.step()
+        self.alg.step()
+        self.alg.step()
+        self.alg.step()
+
+    def test_step_executes_correctly(self):
+        test_cube = cube.Cube()
+        test_cube.move("R")
+        self.alg.step()
+        self.assertEqual(test_cube, self.cube)
+        test_cube.move("U")
+        self.alg.step()
+        self.assertEqual(test_cube, self.cube)
+        test_cube.move("R'")
+        self.alg.step()
+        self.assertEqual(test_cube, self.cube)
+        test_cube.move("U'")
+        self.alg.step()
+        self.assertEqual(test_cube, self.cube)
+
+    def test_algorithm_executes_correctly(self):
+        test_cube = cube.Cube()
+        test_cube.move("R")
+        test_cube.move("U")
+        test_cube.move("R'")
+        test_cube.move("U'")
+        self.alg.execute()
+        self.assertTrue(self.cube == test_cube)
+        self.assertTrue(self.alg.is_finished())
 
 
 if __name__ == '__main__':
